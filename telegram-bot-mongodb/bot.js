3e06f384f8447f5a131ce13e87ac3a3a5aa137bc
Anushka Garg <anushkagarg.int@urbancompany.com>
2024-07-27 20:27:05 +0530

    Revert "bot connecting"
    This reverts commit d69a3e5c31f916785df035997d5a0c3db46a2229.

--- telegram-bot-mongodb/bot.js
+++ telegram-bot-mongodb/bot.js
@@ -1,8 +1,6 @@
 // bot.js
 const TelegramBot = require('node-telegram-bot-api');
 const { connectToDatabase } = require('./db');
-const fs = require('fs');
-const path = require('path');
 
 const token = ''; // Replace with your bot token
 const bot = new TelegramBot(token, { polling: true });
@@ -16,56 +14,12 @@ connectToDatabase().then(db => {
 
         // Initialize or reset user data
         await usersCollection.updateOne(
-            { chat_id: chatId },
-            { $set: { step: 'name', name: '', photo_path: '', location: '' } },
+            { chatId },
+            { $set: { step: 'name', name: '', photo: '', location: '' } },
             { upsert: true }
         );
 
         bot.sendMessage(chatId, 'Hi! Please provide your full name.');
-    });
-
-    bot.on('text', async (msg) => {
-        const chatId = msg.chat.id;
-        const text = msg.text;
-
-        // Get user data
-        const user = await usersCollection.findOne({ chat_id: chatId });
-
-        if (!user) {
-            bot.sendMessage(chatId, 'Please start the conversation by sending /start.');
-            return;
-        }
-
-        if (user.step === 'name') {
-            // Save the name and request the image
-            await usersCollection.updateOne(
-                { chat_id: chatId },
-                { $set: { name: text, step: 'photo' } }
-            );
-            bot.sendMessage(chatId, 'Thank you for providing your name. Now, please send me an image.');
-        } else if (user.step === 'photo') {
-            // Handle photo
-            bot.sendMessage(chatId, 'Please send me an image.');
-        } else if (user.step === 'location') {
-            // Handle location
-            const locationRegex = /^-?\d+\.\d+,-?\d+\.\d+$/;
-            if (locationRegex.test(text)) {
-                // Save location and complete the process
-                await usersCollection.updateOne(
-                    { chat_id: chatId },
-                    { $set: { location: text, step: 'completed' } }
-                );
-                bot.sendMessage(chatId, 'Thank you! We have received your name, image, and location. We will get back to you soon with further updates.');
-
-                // Optionally save user data to a file or database here
-                const userData = `Name: ${user.name}\nLocation: ${user.location}\nImage Path: ${user.photo_path}\n`;
-                fs.writeFileSync(path.join(__dirname, `${chatId}_info.txt`), userData);
-            } else {
-                bot.sendMessage(chatId, 'Please provide a valid location in the format "latitude,longitude".');
-            }
-        } else {
-            bot.sendMessage(chatId, 'I am not sure what you mean. Please follow the instructions.');
-        }
     });
 
     bot.on('photo', async (msg) => {
@@ -75,10 +29,46 @@ connectToDatabase().then(db => {
         const fileLink = await bot.getFileLink(fileId);
         // Save fileLink to database and update user step
         await usersCollection.updateOne(
-            { chat_id: chatId },
-            { $set: { photo_path: fileLink, step: 'location' } }
+            { chatId },
+            { $set: { photo: fileLink, step: 'location' } }
         );
 
         bot.sendMessage(chatId, 'Thank you for sending the image. Please provide your location in the format "latitude,longitude".');
+    });
+
+    bot.onText(/.+/, async (msg) => {
+        const chatId = msg.chat.id;
+        const text = msg.text;
+
+        // Get user data
+        const user = await usersCollection.findOne({ chatId });
+
+        if (!user) {
+            bot.sendMessage(chatId, 'Please start the conversation by sending /start.');
+            return;
+        }
+
+        if (user.step === 'name') {
+            // Save the name and request the image
+            await usersCollection.updateOne(
+                { chatId },
+                { $set: { name: text, step: 'photo' } }
+            );
+            bot.sendMessage(chatId, 'Thank you for providing your name. Now, please send me an image.');
+        } else if (user.step === 'location') {
+            // Handle location
+            if (/^-?\d+\.\d+,-?\d+\.\d+$/.test(text)) {
+                // Save location and complete the process
+                await usersCollection.updateOne(
+                    { chatId },
+                    { $set: { location: text, step: 'completed' } }
+                );
+                bot.sendMessage(chatId, 'Thank you! We have received your name, image, and location. We will get back to you soon with further updates.');
+            } else {
+                bot.sendMessage(chatId, 'Please provide a valid location in the format "latitude,longitude".');
+            }
+        } else {
+            bot.sendMessage(chatId, 'I am not sure what you mean. Please follow the instructions.');
+        }
     });
 });
